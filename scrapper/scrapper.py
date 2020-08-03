e1009d99aa1c78eecc8a5fc187e4e5144075c6d9
Leonard Thong <thongyixuen@gatech.edu>
2020-08-03 11:49:33 -0400

    updating scrapper functionalities

---
+++ scrapper/scrapper.py
@@ -0,0 +1,97 @@
+import datetime
+import numpy as np
+import requests
+
+from collections import Counter
+from dateutil.parser import parse
+from selenium import webdriver
+
+def grab_html():
+    url = 'https://www.reddit.com/r/wallstreetbets/search/?q=flair%3A%22Discussion%22&restrict_sr=1'
+    driver = webdriver.Chrome()
+    driver.get(url)
+    return driver
+
+def grab_stocklink(driver):
+    links = driver.find_elements_by_xpath('//*[@class="_eYtD2XCVieq6emjKBH3m"]')
+    link = ""
+
+    for a in links:
+        if a.text.startswith('Daily Discussion Thread'):
+            date = "".join(a.text.split(' ')[-3:])
+            parsed = parse(date)
+            yesterday = datetime.date.today() - datetime.timedelta(days=1)
+
+            if parse(str(yesterday)) == parsed:
+                link = a.find_element_by_xpath('../..').get_attribute('href')
+
+        if a.text.startswith('Weekend'):
+            date = "".join(a.text.split(' ')[-3:])
+            parsed = parse(date)
+            yesterday = datetime.date.today() - datetime.timedelta(days=3)
+
+            if parse(str(yesterday)) == parsed:
+                link = a.find_element_by_xpath('../..').get_attribute('href')
+
+    stock_link = link.split('/')[-3]
+    driver.close()
+
+    return stock_link
+
+def grab_stocks():
+    with open('tickers.txt', 'r') as w:
+        stocks = w.readlines()
+        stocks_list = []
+        for a in stocks:
+            a = a.replace('\n','')
+            stocks_list.append(a)
+        return stocks_list
+
+def grab_comment_id_list(stock_link):
+    html = requests.get(f'https://api.pushshift.io/reddit/submission/comment_ids/{stock_link}')
+    raw_comment_id_list = html.json()
+    return raw_comment_id_list
+
+def get_comments(comment_id_list):
+    html = requests.get(f'https://api.pushshift.io/reddit/comment/search?ids={comment_id_list}&fields=body&size=1000')
+    new_comments = html.json()
+    return new_comments
+
+def get_all_comments():
+    list = np.array(raw_comment_id_list['data'])
+    comments = {'data': []}
+
+    i = 0
+    while i < len(list):
+        print(len(list))
+
+        new_comments_list = ",".join(list[0:1000])
+        new_comments = get_comments(new_comments_list)
+
+        comments['data'] += new_comments['data']
+
+        remove_me = slice(0, 1000)
+        list = np.delete(list, remove_me)
+
+    return comments
+
+def get_stock_count(comments, stocks_list):
+    stock_dict = Counter()
+
+    for a in comments['data']:
+        for ticker in stocks_list:
+            if ticker in a['body'].split():
+                stock_dict[ticker] += 1
+    stock_dict = dict(stock_dict)
+    return stock_dict
+
+if __name__ == "__main__":
+    driver = grab_html()
+    stock_link = grab_stocklink(driver)
+    stocks_list = grab_stocks()
+
+    raw_comment_id_list = grab_comment_id_list(stock_link)
+    comments = get_all_comments()
+
+    stock_count = get_stock_count(comments, stocks_list)
+    print(stock_count)
