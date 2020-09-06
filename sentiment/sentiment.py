a067be25baf1236813eed783bcfeda20e83238bb
Leonard <thongyixuen@gmail.com>
2020-09-06 11:05:53 -0400

    sentimental analysis with nltk vader

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import nltk
+
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
 
 
 def get_comments():
@@ -12,17 +15,38 @@ def get_comments():
 
     return array
 
+def get_tickers():
+    a = set()
+    file = "comments.csv"
+    df = pd.read_csv(file)
 
-def sentiment_analyse(sentiment_text):
-    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
-    if score['neg'] > score['pos']:
-        print("Negative Sentiment")
-    elif score['neg'] < score['pos']:
-        print("Positive Sentiment")
-    else:
-        print("Neutral Sentiment")
+    for row in df["ticker"]:
+        a.add(row)
+
+    return list(a)
+
+
+def sentiment_analysis(comments):
+    index = 0
+    for comment in comments:
+        score = SentimentIntensityAnalyzer().polarity_scores(comment)
+
+        if score['neg'] > score['pos'] and score['neg'] + .68 > score['neu']:
+            print("Negative Sentiment")
+            index += 1
+        elif score['neg'] < score['pos'] and score['neu'] < score['pos'] + .68:
+            print("Positive Sentiment")
+            index += 1
+        else:
+            print("Neutral Sentiment")
+            index += 1
+
+
 
 
 if __name__ == "__main__":
     comments = get_comments()
+    tickers = get_tickers()
+
+    sentiment_analysis(comments)
 
