016685f7bb612081c5edaa8c7d8493fd0687662a
Leonard <thongyixuen@gmail.com>
2020-12-26 06:50:31 -0500

    switch from mongoose to mongodb

--- server/database.js
+++ server/database.js
@@ -1,26 +1,14 @@
-const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
 
 let jsonData = require("../config/config.json");
-let mongoURI = `mongodb+srv://${jsonData["database"]["username"]}:${jsonData["database"]["password"]}@cluster0.qxcsy.mongodb.net/<dbname>?retryWrites=true&w=majority`
+let mongoURI = `mongodb+srv://${jsonData["database"]["username"]}:${jsonData["database"]["password"]}@cluster0.qxcsy.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(
-            mongoURI,
-            {
-                useNewUrlParser: true,
-                useCreateIndex: true,
-                useFindAndModify: false,
-                useUnifiedTopology: true,
-            }
-        );
-
-        console.log("MongoDB Connected...");
-    } catch (err) {
-        console.error(err.message);
-        // Exit process with failure
-        process.exit(1);
-    }
+    await MongoClient.connect(mongoURI, { useUnifiedTopology: true })
+        .then((client) => {
+            console.log("Connected to MongoDB...");
+        })
+        .catch((error) => console.error(error));
 };
 
 module.exports = connectDB;
