a316f71b52804771b5fb44445c98e8962b3d4adc
Leonard <thongyixuen@gmail.com>
2020-12-26 08:23:26 -0500

    serving data from mongodb

--- server/server.js
+++ server/server.js
@@ -1,7 +1,33 @@
-const express = require('express');
-const connectDB = require('./database');
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+
+let jsonData = require("../config/config.json");
+let mongoURI = `mongodb+srv://${jsonData["database"]["username"]}:${jsonData["database"]["password"]}@cluster0.qxcsy.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
 const app = express();
+
+const connectDB = async () => {
+    await MongoClient.connect(mongoURI, { useUnifiedTopology: true })
+        .then((client) => {
+            var db = client.db("results");
+
+            var query = { _id: "20201226" };
+            var result;
+            db.collection("results")
+                .find(query)
+                .toArray(function (err, result) {
+                    if (err) throw err;
+
+                    result = result;
+                });
+
+            console.log("Connected to MongoDB...");
+            app.use("/", (req, res) => {
+                res.json(result);
+            });
+        })
+        .catch((error) => console.error(error));
+};
 
 // Connect Database
 connectDB();
